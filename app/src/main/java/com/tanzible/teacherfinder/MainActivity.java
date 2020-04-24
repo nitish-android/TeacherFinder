de3e75c06080f861d2e0cff82752c87e81f801a4
Nitish Kumar <>
2020-04-24 22:42:29 +0530

    Initial Commit

---
+++ app/src/main/java/com/tanzible/teacherfinder/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tanzible.teacherfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
